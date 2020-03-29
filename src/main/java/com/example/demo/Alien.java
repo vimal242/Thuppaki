71db5f17a0a63131737b48e54d3ae7f04b9b4b46
Vimal Kumar <vimal242@gmail.com>
2020-03-29 22:54:09 +0530

    initial code commit

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+	
+	Alien()
+	{
+		System.out.println("called");
+	}
+	
+	private int id;
+	private String name;
+	@Autowired
+	private Laptop laptop;
+	
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+ public void show()
+ { System.out.println("show"); 
+ laptop.compile();
+ }
+}
